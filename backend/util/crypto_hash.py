bf90bdbf3722367febca3af2af49f65fb518b4d1
Nanaya <nanayamiki@gmail.com>
2024-06-19 16:26:25 -0700

    create and test the wallet class

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -12,7 +12,11 @@ def crypto_hash(*args):
 
 	# print(f'join_data: {joined_data}')
 
-	return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
+	return hashlib.sha256(joined_data.encode('utf-8')).hexdigest() 
+	# sha256()--> 256 of, 0 and 1, bits. hexdigest() -->64 char so every 4 bits is represented by 1 hexdigit(anything from 0 to 9 and A to F)
+	# it is a one way function from data to sha256.  meaning it can generate a hash from
+	# the data but you can't decrypt that hash and get the original data back just by looking at it.
+	# So it only works one way from the data to the hash if you have the resulting hash.
 
 
 def main():
