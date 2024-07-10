a08148639426348512728c0dda1ca2118503e309
Nanaya <nanayamiki@gmail.com>
2024-07-10 06:59:50 -0700

    react, react hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,38 @@
 import "./main.css";
+import React, { useState } from "react";
+import Joke from "./components/Joke";
 
 function App() {
-	return <div className="App">App</div>;
+	const [userQuery, setUserQuery] = useState("");
+
+	const updateUserQuery = (event) => {
+		console.log("userQuery", userQuery);
+		setUserQuery(event.target.value);
+	};
+
+	const searchQuery = () => {
+		window.open(`https://google.com/search?q=${userQuery}`);
+	};
+
+	const handleKeyUp = (event) => {
+		if (event.key === "Enter") {
+			searchQuery();
+		}
+	};
+
+	return (
+		<div>
+			<input
+				value={userQuery}
+				onChange={updateUserQuery}
+				onKeyUp={handleKeyUp}
+			/>
+			<button onClick={searchQuery}>Search</button>
+			<div>{userQuery}</div>
+			<hr />
+			<Joke />
+		</div>
+	);
 }
 
 export default App;
