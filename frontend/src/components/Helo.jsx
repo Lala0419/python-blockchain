8c88625b15303195e54658a5455666db98a77378
Nanaya <nanayamiki@gmail.com>
2024-07-11 16:05:29 -0700

    move the helo title to the top

--- frontend/src/components/Helo.jsx
+++ frontend/src/components/Helo.jsx
@@ -3,12 +3,12 @@ import { NinjaCanvas } from "./canvas";
 const Helo = () => {
 	return (
 		<>
+			<h3>Welcome to NaynayChain</h3>
 			<section
 				className={`px-6 pb-[15.5rem] md:pb-[22rem] lg:pb-[14rem] max-w-[100vw] h-[50rem]`}
 			>
 				<NinjaCanvas />
 			</section>
-			<h3>Welcome to NaynayChain</h3>
 		</>
 	);
 };
